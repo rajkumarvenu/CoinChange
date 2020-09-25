63875ad2a436794e32e1aeb1b7dcd520be0b88c4
Venugopal <rajkumar.venugopal@delta.com>
2020-09-25 09:50:01 -0400

    Added Test class

--- CoinChangeDemo/src/test/java/com/adp/demo/coinchange/CoinChangeServiceTest.java
+++ CoinChangeDemo/src/test/java/com/adp/demo/coinchange/CoinChangeServiceTest.java
@@ -1,5 +1,24 @@
 package com.adp.demo.coinchange;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.adp.demo.coinchange.common.CoinChangeException;
+
 public class CoinChangeServiceTest {
+	
+	private CoinChangeService service;	
+	
+	@Test
+	public void testGetMinCoinCount() throws CoinChangeException {
+		service = mock(CoinChangeService.class);
+		when(service.getMinCoinCount(Mockito.anyInt())).thenReturn(10);
+		assertEquals(10,service.getMinCoinCount(Mockito.anyInt()));
+	}
 
 }
